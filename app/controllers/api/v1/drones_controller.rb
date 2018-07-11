d55cb303db81a0d97b1083546888d0e0c6774d09
cupofjoy <eelantung@gmail.com>
2018-07-11 11:30:20 -0400

    cleaned up backend

--- app/controllers/api/v1/drones_controller.rb
+++ app/controllers/api/v1/drones_controller.rb
@@ -1,27 +1,6 @@
 class Api::V1::DronesController < ApplicationController
   def index
     drones = Drone.all
-    objs = drones.map do |drone|
-      {
-        country: drone.country,
-        town: drone.town,
-        date: drone.date,
-        narrative: drone.narrative,
-        location: drone.location,
-        deaths: drone.deaths,
-        civilians: drone.civilians,
-        children: drone.children,
-        injuries: drone.injuries,
-        tweet_id: drone.tweet_id,
-        bureau_id: drone.bureau_id,
-        bij_summary_short: drone.bij_summary_short,
-        bij_link: drone.bij_link,
-        target: drone.target,
-        lon: drone.lon,
-        lat: drone.lat,
-        names: drone.names
-      }
-    end
     render json: drones
   end
 
